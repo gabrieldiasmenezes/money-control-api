11af88d279e1093e0ccdfb3259da96c16ce4bcf0
gabrieldiasmenezes <g.dias120306@gmail.com>
2025-04-07 09:40:09 -0300

    Ads Transaction model,repositiry and controller with seeding logic

--- src/main/java/br/com/fiap/money_control_api/config/DataBaseSeeder.java
+++ src/main/java/br/com/fiap/money_control_api/config/DataBaseSeeder.java
@@ -1,12 +1,18 @@
 package br.com.fiap.money_control_api.config;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.com.fiap.money_control_api.model.Category;
+import br.com.fiap.money_control_api.model.Transaction;
 import br.com.fiap.money_control_api.repository.CategoryRepository;
+import br.com.fiap.money_control_api.repository.TransactionRepository;
 import jakarta.annotation.PostConstruct;
 
 @Component
@@ -14,16 +20,35 @@ public class DataBaseSeeder {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+    
     @PostConstruct
     public void init() {
-        categoryRepository.saveAll(
-                List.of(
-                    Category.builder().name("Educação").icon("Book").build(),
-                    Category.builder().name("Lazer").icon("Dices").build(),
-                    Category.builder().name("Transporte").icon("Bus").build()
-                    
-                    ));
-    }
+        var categories=List.of(
+                Category.builder().name("Educação").icon("Book").build(),
+                Category.builder().name("Lazer").icon("Dices").build(),
+                Category.builder().name("Transporte").icon("Bus").build()         
+                );
+        categoryRepository.saveAll(categories);
+        var descriptions=List.of("Corrida Uber","Remédio","Café Especial","Livro didático","Cinema","Bilhete Único","Restaurante","Faculdade","Plano de Saúde","Conta de luz");
+        var transactions=new ArrayList<Transaction>();
+        for (int i = 0; i < 50; i++) {
+            transactions.add(
+                Transaction.builder()
+                    .description(descriptions.get(new Random().nextInt(descriptions.size())))
+                    .amount(BigDecimal.valueOf(10+new Random().nextDouble()*500))
+                    .date(LocalDate.now().minusDays(1))
+                    .type("out")
+                    .category(categories.get(new Random().nextInt(categories.size())))
+                    .build()
+            );
+            
+        }
+        transactionRepository.saveAll(transactions);
+        
+    };
+
    
     
 }
---
+++ src/main/java/br/com/fiap/money_control_api/repository/TransactionRepository.java
@@ -0,0 +1,16 @@
+package br.com.fiap.money_control_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.fiap.money_control_api.model.Transaction;
+
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    //List<Transaction> findByDescriptionContainingIgnoringCase(String description);
+
+    //List<Transaction> findByDescriptionContainingIgnoringCaseAndDate(String description, LocalDate date);
+
+   //List<Transaction> findByDate(LocalDate date);
+    
+}
